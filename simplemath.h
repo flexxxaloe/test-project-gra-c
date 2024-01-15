b7307c34f6f20e6d5b4648a2e1d1f17a35d265f9
Konstantin <flexxxaloe2000@gmail.com>
2024-01-15 16:31:44 +0100

    Первый коммит

--- simplemath.h
+++ simplemath.h
@@ -5,7 +5,7 @@
 #ifndef UNTITLED2_SIMPLEMATH_H
 #define UNTITLED2_SIMPLEMATH_H
 
-float sqrtfunc(double n);
+float sqrtfunc(float n);
 float power(float base, int exponent);
 
 #endif //UNTITLED2_SIMPLEMATH_H
