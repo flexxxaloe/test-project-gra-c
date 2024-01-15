167d95e47427a480687b4400e08915653cf5a5e9
Konstantin <flexxxaloe2000@gmail.com>
2024-01-15 16:44:38 +0100

    Первый коммит

--- image_processing.c
+++ image_processing.c
@@ -65,31 +65,6 @@ void adjust_contrast(Image *image) {
         image->pixels[i].b = clamp((int)adjusted_brightness, 0, 255);
     }
 }
-
-/*
-void adjust_brightness_contrast(const Image* grayscale_img, int16_t brightness, float contrast, Image* result_img) {
-    // Реализация корректировки яркости и контраста
-    size_t num_pixels = grayscale_img->width * grayscale_img->height;
-
-    for (size_t i = 0; i < num_pixels; ++i) {
-        // Извлечение значения оттенка серого
-        uint8_t grayscale_value = grayscale_img->pixels[i].r; // Предполагается, что изображение уже в оттенках серого
-
-        // Корректировка яркости
-        int16_t adjusted_brightness = (int16_t)grayscale_value + brightness;
-        adjusted_brightness = (adjusted_brightness < 0) ? 0 : ((adjusted_brightness > 255) ? 255 : adjusted_brightness);
-
-        // Корректировка контраста
-        float adjusted_contrast = contrast * (adjusted_brightness - 128) + 128;
-        adjusted_contrast = (adjusted_contrast < 0) ? 0 : ((adjusted_contrast > 255) ? 255 : adjusted_contrast);
-
-        // Запись значения в новое изображение с корректировкой яркости и контраста
-        result_img->pixels[i].r = (uint8_t)adjusted_contrast;
-        result_img->pixels[i].g = (uint8_t)adjusted_contrast;
-        result_img->pixels[i].b = (uint8_t)adjusted_contrast;
-    }
-}
- */
 void simple_adjust_contrast(Image *image) {
     float contrast = image->contrast;
     // Реализация коррекции контраста
@@ -123,3 +98,28 @@ void simple_adjust_contrast(Image *image) {
         image->pixels[i].b = clamp((int)adjusted_brightness, 0, 255);
     }
 }
+
+/*
+void adjust_brightness_contrast(const Image* grayscale_img, int16_t brightness, float contrast, Image* result_img) {
+    // Реализация корректировки яркости и контраста
+    size_t num_pixels = grayscale_img->width * grayscale_img->height;
+
+    for (size_t i = 0; i < num_pixels; ++i) {
+        // Извлечение значения оттенка серого
+        uint8_t grayscale_value = grayscale_img->pixels[i].r; // Предполагается, что изображение уже в оттенках серого
+
+        // Корректировка яркости
+        int16_t adjusted_brightness = (int16_t)grayscale_value + brightness;
+        adjusted_brightness = (adjusted_brightness < 0) ? 0 : ((adjusted_brightness > 255) ? 255 : adjusted_brightness);
+
+        // Корректировка контраста
+        float adjusted_contrast = contrast * (adjusted_brightness - 128) + 128;
+        adjusted_contrast = (adjusted_contrast < 0) ? 0 : ((adjusted_contrast > 255) ? 255 : adjusted_contrast);
+
+        // Запись значения в новое изображение с корректировкой яркости и контраста
+        result_img->pixels[i].r = (uint8_t)adjusted_contrast;
+        result_img->pixels[i].g = (uint8_t)adjusted_contrast;
+        result_img->pixels[i].b = (uint8_t)adjusted_contrast;
+    }
+}
+ */
--- simplemath.c
+++ simplemath.c
@@ -3,19 +3,20 @@
 
 
 // from https://ru.stackoverflow.com/questions/566475/%D0%92%D1%8B%D1%87%D0%B8%D1%81%D0%BB%D0%B5%D0%BD%D0%B8%D0%B5-%D0%BA%D0%B2%D0%B0%D0%B4%D1%80%D0%B0%D1%82%D0%BD%D0%BE%D0%B3%D0%BE-%D0%BA%D0%BE%D1%80%D0%BD%D1%8F-%D0%B1%D0%B5%D0%B7-%D0%B1%D0%B8%D0%B1%D0%BB%D0%B8%D0%BE%D1%82%D0%B5%D1%87%D0%BD%D1%8B%D1%85-%D0%BC%D0%B5%D1%82%D0%BE%D0%B4%D0%BE%D0%B2
-float sqrtfunc(float n) {
-    float l = 0;
-    float r = 1e30;  // Большое число для float
-    float m;
+//from https://en.wikipedia.org/wiki/Fast_inverse_square_root
+float sqrtfunc(float number) {
+    long i;
+    float x2, y;
+    const float threehalfs = 1.5F;
 
-    while (r - l > 1e-4) {  // Точность для float
-        m = l + (r - l) / 2;
-        if (m * m > n)
-            l = m;
-        else
-            r = m;
-    }
-    return m;
+    x2 = number * 0.5F;
+    y  = number;
+    i  = * ( long * ) &y;
+    i  = 0x5f3759df - ( i >> 1 );
+    y  = * ( float * ) &i;
+    y  = y * ( threehalfs - ( x2 * y * y ) );   // 1 итерация
+//  y  = y * ( threehalfs - ( x2 * y * y ) );   // 2 итерация, можно удалить
+    return 1/y;
 }
 
 float power(float base, int exponent) {
